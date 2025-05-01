03e206df395648f8653f5e878f1fca0a7ca193f1
Javier Andres Ferreyra Vagliati <Javier.FerreyraVagliati@alu.frt.utn.edu.ar>
2025-05-01 20:49:10 -0300

    Se realizaron del punto 1 al 6 del tp8

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    class CajaDeAhorro : CuentaBancaria
+    {
+        public decimal TasaDeInteres { get; set; }
+        public CajaDeAhorro(decimal tasaDeInteres, string numero, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
+        {
+            TasaDeInteres = tasaDeInteres;
+        }
+        public override void Depositar(decimal monto)
+        {
+            Saldo += monto;
+
+        }
+
+        public override void Retirar(decimal monto)
+        {
+
+            Saldo -= monto;
+
+        }
+
+        public void AplicarInteres()
+        {
+            Saldo += Saldo * TasaDeInteres;
+
+        }
+    }
+
+
+
+}
---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    class CuentaCorriente : CuentaBancaria
+    {
+        private decimal _limiteDeDescubierto;
+        private decimal _comision;
+        public CuentaCorriente(decimal limiteDeDescubierto, decimal comision, string numero, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
+        {
+            _limiteDeDescubierto = limiteDeDescubierto;
+            _comision = comision;
+        }
+
+        public override void Depositar(decimal monto)
+        {
+            monto -= monto * _comision;
+            saldo += monto;
+
+        }
+
+        public override void Retirar(decimal monto)
+        {
+
+            if (saldo - monto >= -_limiteDeDescubierto)
+            {
+                saldo -= monto;
+            }
+            if (saldo < 0)
+            {
+                estado = Estado.Suspendida;
+            }
+        }
+        public decimal GetLimiteDeDescubierto()
+        {
+            return _limiteDeDescubierto;
+        }
+
+        public void SetLimiteDeDescubierto(decimal limiteDeDescubierto)
+        {
+            _limiteDeDescubierto = limiteDeDescubierto;
+        }
+
+        public decimal GetComision()
+        {
+            return _comision;
+        }
+
+        public void SetComision(decimal comision)
+        {
+            _comision = comision;
+        }
+    }
+}
